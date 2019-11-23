f3bc1fd691d8d3dd114187ed089bb6de168fc8aa
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2019-11-22 21:31:56 -0400

    Test de usuario creado.

---
+++ ProyectoFinalAp1Tests/BLL/RepositorioUsuarioTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinalAp1.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAp1.BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioUsuarioTests
+    {
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioUsuario repositorio = new RepositorioUsuario();
+            Assert.IsTrue(repositorio.Eliminar(4));
+        }
+    }
+}
